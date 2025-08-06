ab8127ca5de0d006f943546ed1416de204e7c423
Huntington <cohuntington7@gmail.com>
2025-08-06 16:15:29 -0700

    Challenge 5 complete

--- index.js
+++ index.js
@@ -96,10 +96,11 @@ function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have not yet met their goal
-
+    let underFundedGames = GAMES_JSON.filter((game) => { return game.pledged < game.goal; });
+    console.log(`Games that hasn't met funding goals: ${underFundedGames.length}`);
 
     // use the function we previously created to add the unfunded games to the DOM
-
+    addGamesToPage(underFundedGames);
 }
 
 // show only games that are fully funded
@@ -107,10 +108,11 @@ function filterFundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have met or exceeded their goal
-
+    let fullyFundedGames = GAMES_JSON.filter ( (game) => { return game.pledged >= game.goal; });
+    console.log(`Games that met funding goals: ${fullyFundedGames.length}`);
 
     // use the function we previously created to add unfunded games to the DOM
-
+    addGamesToPage(fullyFundedGames);
 }
 
 // show all games
@@ -118,7 +120,7 @@ function showAllGames() {
     deleteChildElements(gamesContainer);
 
     // add all games from the JSON data to the DOM
-
+    addGamesToPage(GAMES_JSON);
 }
 
 // select each button in the "Our Games" section
@@ -127,7 +129,9 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
-
+unfundedBtn.addEventListener("click", filterUnfundedOnly);
+fundedBtn.addEventListener("click", filterFundedOnly);
+allBtn.addEventListener("click", showAllGames);
 
 /*************************************************************************************
  * Challenge 6: Add more information at the top of the page about the company.
